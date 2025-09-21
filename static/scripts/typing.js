1b84b5c99fdc0be9b40fbccde5a2789e9fa70025
Pixena <rustem4uz.dev@gmail.com>
2025-09-21 19:30:06 +0500

    Initial commit: 21.09.25 version 1.0

---
+++ static/scripts/typing.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#typed', {
+  strings: [
+    'разработкой веб сайтов',
+    'вебдизайном',
+    'созданием чат-ботов',
+    'фрилансом'
+  ],
+  typeSpeed: 40,
+  backSpeed: 20,
+  backDelay: 3500,
+  loop: true,
+  showCursor: true,
+  cursorChar: '|'
+});
